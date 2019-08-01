45d1364a8c73570206f6d04513768f5b5440225c
Albert Tedja <albert@siliconaxon.com>
2019-08-01 05:32:55 +0000

    Remove new ambiguity

--- merge.go
+++ merge.go
@@ -2,14 +2,14 @@ package gmap
 
 // MergeFunc determines how to merge two maps together when there is a key collision.
 // Values from both maps are passed to the function, with old represents the value from the Map being merged into, and new is the value from the other Map.
-type MergeFunc func(k string, old, new interface{}) interface{}
+type MergeFunc func(k string, oldValue, newValue interface{}) interface{}
 
 // Merges this map with another Map.
 // Entries with key collisions are overwritten with the values from other Map.
 // Returns a new Map.
 func (m Map) Merge(other Map) Map {
-	return m.MergeWithFunc(other, func(k string, old, new interface{}) interface{} {
-		return new
+	return m.MergeWithFunc(other, func(k string, oldValue, newValue interface{}) interface{} {
+		return newValue
 	})
 }
 
